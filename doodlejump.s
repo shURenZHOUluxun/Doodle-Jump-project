f9ed5b0386cbb3e0d42e6e6dc78322b4b6d767b4
shURenZHOUluxun <zengjiayifabe@outlook.com>
2020-12-09 15:16:31 -0500

    edit doodle jump game

--- doodlejump.s
+++ doodlejump.s
@@ -33,40 +33,54 @@
 	green: .word 0x00ff00
 	black: .word 0x000000
 	platformArray: .word 0:5 #initialize array of 5 integers to store position of platform
+	doodler: .word 496 #initial offset of position of doodler
 .text
 main:		lw $t0, displayAddress # $t0 stores the base address for display
-		lw $t1, red # $t1 stores the red colour code
-		lw $t2, green # $t2 stores the green colour code
-		lw $t3, black # $t3 stores the black colour code
 		addi $sp, $sp, -4
 		sw $ra, 0($sp) #store return address into stack 
-		jal drawplatform #initialize 5 platform randomly
+		jal platformLocation # calculate new platform location
+		jal drawplatform #initialize 5 platform based on platformLocation
+goUp:		lw, $t2, doodler
+		ble $t2, 0, Terminate #if doodler exceeds 
 		jal drawdoodler #initialize doodler to the center of the screen
-		 
-
-		li $v0, 10 # terminate the program gracefully
+		jal collision # check collision
+		lw $t1, 0($sp)
+		beq $t1, 1 , goUp
+dropagain:	jal dropdoodler #doodler drop by one unit
+		jal collision # check collision
+		lw $t1, 0($sp)
+		beq $t1, 1 , goUp
+		lw $t2, doodler
+		blt $t2, 1024, dropagain
+Terminate:	li $v0, 10 # terminate the program gracefully
 		syscall
 
-		
+
+platformLocation:	li $t1, 5 #loop 5 times
+			li $t2, 0 #offset
+			la $t6, platformArray #load array address
+loop:			li $v0, 42 #radomize a number
+			li $a0, 0 
+			li $a1, 1024 #randomized number will be in range 0-1024
+			syscall # radomized number stored in $a0
+			sll $t5, $t2, 2 
+			add $t6, $t6, $t5 #locate to array where we want to store number in 
+			sw $a0, 0($t6) #store number into array
+			addi $t2, $t2, 1 #offset add 1
+			bne $t2, $t1, loop #check if we have 5 numbers already
+			jr $ra	#jump back to caller
 		
 drawplatform:	li $t5, 4 #t5 stores 4
 		li $t4, 5 #loop number, loop 5 times
 		li $t7, 0 #offset for platform array address
-loop:		li $v0, 42 #radomize a number
-		li $a0, 0 
-		li $a1, 1024 #randomized number will be in range 0-1024
-		syscall # radomized number stored in $a0
 		la $t6, platformArray #load platformArray address
-		sll $t8, $t7, 2 #offset $t7 times 4
+loop2:		sll $t8, $t7, 2 #offset $t7 times 4
 		add $t6, $t6, $t8 #locate to array where we want to store number in 
-		sw $a0, 0($t6) #store randomised position into array
-		mul $a0, $a0, $t5 #multiply radomized number with 4
-		
-		lw $t3, black #store black color
-		lw $t2, green #store green color4
+		lw $t3, 0($t6) #load position from array
+		mul $t3, $t3, $t5 #multiply position number with 4
 		lw $t1, red #store red color
 		lw $t0, displayAddress #store base address
-		add $t0, $t0, $a0 #calculate position
+		add $t0, $t0, $t3 #calculate position
 		sw $t1, 0($t0) # paint the platform by red 
 		sw $t1, 4($t0) 
 		sw $t1, 8($t0) 
@@ -74,17 +88,19 @@ loop:		li $v0, 42 #radomize a number
 		sw $t1, 16($t0)
 		sw $t1, 20($t0) #the platform has length of 6 unit 
 		addi, $t7, $t7, 1 # increase $t7 by 1
-		bne, $t7, $t4, loop #if not complete 5 loops, continue looping
+		bne, $t7, $t4, loop2 #if not complete 5 loops, continue looping
 END:		jr $ra#jump back to main
 
 				
 drawdoodler:	lw $t3, black #store black color
-		lw $t2, green #store green color4
+		lw $t2, green #store green color
 		lw $t1, red #store red color
 		lw $t0, displayAddress #store base address
+		li $t6, 4 # store 4
 		li $t5, 10 #move up 10 units
-		li $t4, 1984 
-loop2:		lw $t0, displayAddress #store base address
+loop3:		lw $t4, doodler
+		mul $t4, $t4, $t6 #multiply $t4 with 4
+		lw $t0, displayAddress #store base address
 		add $t0, $t0, $t4 #calculate position
 		sw $t2, 0($t0) # paint the doodler by red 
 		sw $t2, 124($t0) 
@@ -102,7 +118,7 @@ loop2:		lw $t0, displayAddress #store base address
 		sw $t1, 512($t0)
 		sw $t1, 516($t0)
 		li $v0, 32 # wait for 1 second
-		li $a0, 1000
+		li $a0, 100
 		syscall
 		sw $t3, 0($t0) # repaint the screen by black
 		sw $t3, 124($t0) 
@@ -119,12 +135,85 @@ loop2:		lw $t0, displayAddress #store base address
 		sw $t3, 508($t0)
 		sw $t3, 512($t0)
 		sw $t3, 516($t0)
+		addi $sp, $sp, -4
+		sw $ra, 0($sp) #store return address into stack 
+		addi $sp, $sp, -4
+		sw $t0, 0($sp) #store $t0 into stack 
+		addi $sp, $sp, -4
+		sw $t1, 0($sp) #store $t1 into stack 
+		addi $sp, $sp, -4
+		sw $t2, 0($sp) #store $t2 into stack 
+		addi $sp, $sp, -4
+		sw $t3, 0($sp) #store $t3 into stack 
+		addi $sp, $sp, -4
+		sw $t4, 0($sp) #store $t4 into stack 
+		addi $sp, $sp, -4
+		sw $t5, 0($sp) #store $t5 into stack 
+		addi $sp, $sp, -4
+		sw $t6, 0($sp) #store $t6 into stack 
+		jal drawplatform
+		lw $t6, 0($sp) #load $t6 from stack 
+		addi $sp, $sp, 4
+		lw $t5, 0($sp) #load $t5 from stack 
+		addi $sp, $sp, 4
+		lw $t4, 0($sp) #load $t4 from stack 
+		addi $sp, $sp, 4
+		lw $t3, 0($sp) #load $t3 from stack 
+		addi $sp, $sp, 4
+		lw $t2, 0($sp) #load $t2 from stack 
+		addi $sp, $sp, 4
+		lw $t1, 0($sp) #load $t1 from stack 
+		addi $sp, $sp, 4
+		lw $t0, 0($sp) #load $t0 from stack 
+		addi $sp, $sp, 4
+		lw $ra, 0($sp) #load return address into stack 
+		addi $sp, $sp, 4
 		lw $t7, 0xffff0004
 		beq $t7, 0x6a, respond_j # check if j was pressed
 		beq $t7, 0x6b, respond_k # check if k was pressed
 draw_move:	addi $t4, $t4, -128
+		sra $t4, $t4, 2 #divide $t4 by 4
+		la $s0, doodler
+		sw $t4, 0($s0)
 		addi $t5, $t5, -1
-		bnez $t5, loop2
+		bnez $t5, loop3
+		
+		#sw $t2, 0($t0) # paint the doodler 
+		#sw $t2, 124($t0) 
+		#sw $t2, 128($t0) 
+		#sw $t2, 132($t0)
+		#sw $t2, 252($t0)
+		#sw $t2, 256($t0)
+		#sw $t3, 260($t0)
+		#sw $t2, 264($t0)
+		#sw $t2, 268($t0)
+		#sw $t2, 380($t0)
+		#sw $t2, 384($t0)
+		#sw $t2, 388($t0)
+		#sw $t1, 508($t0)
+		#sw $t1, 512($t0)
+		#sw $t1, 516($t0)
+		jr $ra
+		
+
+respond_j:	addi $t4, $t4, -4 # position move one block to left
+		j draw_move
+respond_k:	addi $t4, $t4, 4 # position move one block to right		
+		j draw_move	
+
+dropdoodler:	lw $t3, black #store black color
+		lw $t2, green #store green color
+		lw $t1, red #store red color
+		li $t6, 4 # store 4
+		lw $t4, doodler
+		mul $t4, $t4, $t6 #multiply $t4 with 4
+		addi $t4, $t4, 128
+		lw $t0, displayAddress #store base address
+		add $t0, $t0, $t4 #calculate position
+		
+		sra $t4, $t4, 2 #divide $t4 by 4
+		la $s0, doodler
+		sw $t4, 0($s0)
 		
 		sw $t2, 0($t0) # paint the doodler 
 		sw $t2, 124($t0) 
@@ -141,15 +230,56 @@ draw_move:	addi $t4, $t4, -128
 		sw $t1, 508($t0)
 		sw $t1, 512($t0)
 		sw $t1, 516($t0)
+		li $v0, 32 # wait for 1 second
+		li $a0, 100
+		syscall
+		sw $t3, 0($t0) # repaint the screen by black
+		sw $t3, 124($t0) 
+		sw $t3, 128($t0) 
+		sw $t3, 132($t0)
+		sw $t3, 252($t0)
+		sw $t3, 256($t0)
+		sw $t3, 260($t0)
+		sw $t3, 264($t0)
+		sw $t3, 268($t0)
+		sw $t3, 380($t0)
+		sw $t3, 384($t0)
+		sw $t3, 388($t0)
+		sw $t3, 508($t0)
+		sw $t3, 512($t0)
+		sw $t3, 516($t0)
 		jr $ra
 		
-
-respond_j:	addi $t4, $t4, -4 # position move one block to left
-		j draw_move
-respond_k:	addi $t4, $t4, 4 # position move one block to right		
-		j draw_move	
-	
-	
+collision:	li $t5, 4 #t5 stores 4
+		li $t4, 5 #loop number, loop 5 times
+		li $t7, 0 #offset for platform array address
+		la $t6, platformArray #load platformArray address
+loop4:		sll $t8, $t7, 2 #offset $t7 times 4
+		add $t6, $t6, $t8 #locate to array with offset
+		lw $t3, 0($t6) #load position of platform from array
+		lw $t2, doodler #load doodler position		
+		addi $t2, $t2, 129 #doodler left bottom position
+		bge $t2, $t3, next
+		addi $t7, $t7, 1
+		bne $t7, $t4, loop4
+		li $t0, 0
+		addi $sp, $sp, -4
+		sw $t0, 0($sp) #store 1 for true 0 for false into stack 
+		jr $ra
+next:		addi $t2, $t2, -2
+		addi $t3, $t3, 6
+		ble $t2, $t3, End
+		addi $t7, $t7, 1
+		bne $t7, $t4, loop4
+		li $t0, 0
+		addi $sp, $sp, -4
+		sw $t0, 0($sp) #store 1 for true 0 for false into stack 
+		jr $ra
+		
+End:		li $t0, 1
+		addi $sp, $sp, -4
+		sw $t0, 0($sp) #store 1 for true 0 for false into stack 
+		jr $ra		
 #Exit:
 	#li $v0, 10 # terminate the program gracefully
 	#syscall
